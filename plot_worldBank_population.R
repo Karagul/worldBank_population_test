5e5fb0f1a276beb4f197e868e1901efdd1fb40ea
nequals30 <nequals30@gmail.com>
2017-03-22 22:24:41 -0500

    adding more plotting functions

--- plot_worldBank_population.R
+++ plot_worldBank_population.R
@@ -19,3 +19,24 @@ plot_pie_region<-function(regionType,year){
   
   print(p)
 }
+
+# Plots a stacked area graph of all the regions populations through history ----------------
+plot_stackArea_histByRegion<-function(regionType,yrs){
+  
+  popTbl <- get_worldBank_region_population(regionType,tail(yrs,n=1));
+  
+  regionHistTbl <- data.frame(region=character(),year=integer(),population=numeric());
+  
+  for (i in 1:nrow(popTbl)){
+    thisHist <- get_worldBank_region_popHist(popTbl[i,"region"]);
+    thisHist <- cbind(rep(popTbl[i,"region"],nrow(thisHist)),thisHist);
+    colnames(thisHist)[1] = 'region';
+    regionHistTbl <- rbind(regionHistTbl,thisHist);
+  }
+  
+  regionHistTbl[,"population"] <- regionHistTbl[,"population"]/1000000
+  
+  p <- ggplot(regionHistTbl, aes(x=year, y=population, fill=region)) + geom_area();
+  
+  print(p)
+}
